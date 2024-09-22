de70b6e1d902ce34692c73745ef1281206c5a252
Rodrigo Gama <rodrigogama@greal.com.br>
2024-09-21 22:11:21 -0300

    update: update redis

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { CacheInterceptor, Module } from '@nestjs/common';
+import { CacheInterceptor, CacheModule, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -9,6 +9,7 @@ import { UsersModule } from './modules/users/users.module';
 import { APP_GUARD } from '@nestjs/core';
 import { AuthGuardService } from './modules/auth/auth-guard/auth-guard.service';
 import { AuthModule } from './modules/auth/auth.module';
+import * as redisStore from 'cache-manager-redis-store'; // Importando o redisStore
 
 @Module({
   imports: [
@@ -25,14 +26,20 @@ import { AuthModule } from './modules/auth/auth.module';
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
     }),
+    CacheModule.register({
+      isGlobal: true,
+      store: redisStore,
+      host: process.env.REDIS_HOST,
+      port: +process.env.REDIS_PORT,
+    }),
   ],
   controllers: [AppController],
   providers: [
     AppService,
     {
-    provide: APP_GUARD,
-    useClass: AuthGuardService,
+      provide: APP_GUARD,
+      useClass: AuthGuardService,
     },
-    ],
+  ],
 })
 export class AppModule {}
